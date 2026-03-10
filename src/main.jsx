ca45a4bfbdc3dada7fb1918a9f7e9732eb62b73a
yuossef151 <yuossefgaafar07@gmail.com>
2026-03-11 01:35:56 +0200

    رفع جميع التعديلات الجديدة والمعدلة

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { HashRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
     
-  </HashRouter>
+  
 );
