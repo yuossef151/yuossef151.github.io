cab49ebe3ace817ed872141a6cf4c1561fec1c8d
yuossef151 <yuossefgaafar07@gmail.com>
2026-03-11 01:28:41 +0200

    رفع جميع التعديلات الجديدة والمعدلة

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,15 @@
-git add .
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter } from "react-router-dom";
+
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+    
+  </HashRouter>
+);
